897d479e3582a34dd835ef0b2d9fb27c10dfc522
ViktoriiaZaichuk <61655420+ViktoriiaZaichuk@users.noreply.github.com>
2022-01-19 15:31:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,27 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
 
+// Write your code below:
+const generateTarget = (number) => {
+ return Math.floor(Math.random()*10);
+}
+const compareGuesses = (human, computer, number) => {
+  if (human === true) {
+    return 'The human player win.';
+  } else {
+    return 'The computer player win.';
+  }
+}  
+const updateScore = (string) => {
+ if (string === 'human') {
+   return humanScore++;
+ } else {
+  return computerScore++;
+ }
+}
+
+const advanceRound = () => currentRoundNumber++;
